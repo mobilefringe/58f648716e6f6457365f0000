d6b7f440c3e469b06b0403aab1047bd3234983c1
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-02-08 12:27:07 -0500

    Clairemont Town Square 1.4  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "The District at Tustin Legacy",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/59a082116e6f64581b7e0000/image/jpeg/1517929540000/cts_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
+                }
