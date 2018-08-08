e77d7ac52dda7d670977b2d3f839be0bb5a410cd
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-08-08 10:52:43 -0400

    Clairemont Town Square 1.5  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Clairemont Town Square",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/5b5f65b26e6f644fe86e0200/image/jpeg/1517929540000/cts_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//clairemont.mallmaverick.com/api/v2/clairemont/social.json"
+                }
